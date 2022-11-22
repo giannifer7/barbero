5a0df8d081d228a169c3c228d05f19ce92fef7d2
giannifer7 <gianni.ferrarotti@gmail.com>
2022-11-22 13:51:54 +0100

    import all browsers

--- configuration.py
+++ configuration.py
@@ -4,54 +4,67 @@ and passed to all the top level functions.
 """
 
 
-import os
 import logging
+import os
+from importlib import import_module
 
-from abstract_configuration import AbstractConfiguration
 from abstract_browser import AbstractBrowser
+from abstract_configuration import AbstractConfiguration
 
 
 class Configuration(AbstractConfiguration):
-    def __init__(self, logging_level=logging.WARNING) -> None:
+    """Configuration and logging class, intended to be created by the caller
+    of the library and passed to the library functions.
+    This helps avoiding globals,
+    see https://en.wikipedia.org/wiki/Dependency_injection"""
+
+    def __init__(self, logging_level: int = logging.WARNING) -> None:
+        """Setup the logger, define and create the work_dir."""
+
         self.setup_logging(logging_level)
         self._work_dir = "_work_dir"
         os.makedirs(self._work_dir, exist_ok=True)
+        self.setup_browsers()
 
-    def setup_logging(self, logging_level, logger_name="base") -> None:
+    def setup_browsers(self) -> None:
+        """Dynamically load the browser classes from the browsers package."""
+
+        self._browsers: dict[str, AbstractBrowser] = {}
+        script_dir = os.path.dirname(__file__)
+        browser_modules_dir = "browsers"
+        for file_name in os.listdir(os.path.join(
+                    script_dir,
+                    browser_modules_dir
+                    )):
+            base, ext = os.path.splitext(file_name)
+            if ext != ".py" or base == "__init__":
+                continue
+            try:
+                browser_module = import_module(
+                    f"{browser_modules_dir}.{base}", browser_modules_dir
+                )
+                browser_class = getattr(browser_module, "B" + base)
+                self._browsers[base] = browser_class()
+            except (FileNotFoundError, OSError, AttributeError, RuntimeError):
+                self.logger.exception("Loading %s browser class failed.", base)
+
+    def setup_logging(self, logging_level: int,
+                      logger_name: str = "history") -> None:
+        """Configure a logger to be exposed in the logger property."""
+
         self._logger = logging.getLogger(logger_name)
         self._logger.setLevel(logging.DEBUG)
         file_handler = logging.FileHandler("history.log")
         file_handler.setLevel(logging.DEBUG)
-        # file_handler.setLevel(logging.INFO)
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging_level)
-        # create formatter and add it to the handlers
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
         console_handler.setFormatter(formatter)
         file_handler.setFormatter(formatter)
-        # add the handlers to _logger
         self._logger.addHandler(console_handler)
         self._logger.addHandler(file_handler)
-
-    def debug(self, *args, **kwargs) -> None:
-        self._logger.debug(*args, **kwargs)
-
-    def info(self, *args, **kwargs) -> None:
-        self._logger.info(*args, **kwargs)
-
-    def warning(self, *args, **kwargs) -> None:
-        self._logger.warning(*args, **kwargs)
-
-    def error(self, *args, **kwargs) -> None:
-        self._logger.error(*args, **kwargs)
-
-    def critical(self, *args, **kwargs) -> None:
-        self._logger.critical(*args, **kwargs)
-
-    def work_path(self, f) -> str:
-        return os.path.join(self._work_dir, f)
 
     @property
     def logger(self) -> logging.Logger:
@@ -61,16 +74,9 @@ class Configuration(AbstractConfiguration):
     def work_dir(self) -> str:
         return self._work_dir
 
+    def work_path(self, file_name: str) -> str:
+        return os.path.join(self._work_dir, file_name)
+
     @property
     def browsers(self) -> dict[str, AbstractBrowser]:
-        from browsers.chromium import Bchromium
-        from browsers.falkon import Bfalkon
-        from browsers.firefox import Bfirefox
-        from browsers.opera import Bopera
-
-        return dict(
-            chromium=Bchromium(),
-            firefox=Bfirefox(),
-            falkon=Bfalkon(),
-            opera=Bopera(),
-        )
+        return self._browsers
